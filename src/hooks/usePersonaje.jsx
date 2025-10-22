e3f6fbed51f1da64542835c1d2754466a2fc723c
Fabio DrizZt <ing.fabio.arg@gmail.com>
2025-10-22 20:13:01 -0300

    fix keydowns

--- src/hooks/usePersonaje.jsx
+++ src/hooks/usePersonaje.jsx
@@ -1,47 +1,53 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const URL_BASE = 'https://rickandmortyapi.com/api'
+const URL_BASE = "https://rickandmortyapi.com/api";
 
 export default function usePersonaje() {
-  const [personaje, setPersonaje] = useState({})
-  const [id, setId] = useState(1)
-  const [max, setMax] = useState(826)
+  const [personaje, setPersonaje] = useState({});
+  const [id, setId] = useState(1);
+  const [max, setMax] = useState(0);
 
   useEffect(() => {
     axios(URL_BASE + `/character`)
-      .then(res => setMax(res.data.info.count))
-      .catch(err => console.log(err))
-  }, [])
+      .then((res) => setMax(res.data.info.count))
+      .catch((err) => console.log(err));
+  }, []);
 
   useEffect(() => {
     axios(URL_BASE + `/character/${id}`)
-      .then(res => setPersonaje(res.data))
-      .catch(err => console.log(err))
-  }, [id])
+      .then((res) => setPersonaje(res.data))
+      .catch((err) => console.log(err));
+  }, [id]);
 
   useEffect(() => {
-    window.addEventListener('keydown', (e) => {
-      if (e.key === 'ArrowRight') {
-        siguiente()
+    const handleKeyDown = (e) => {
+      if (e.key === "ArrowRight") {
+        siguiente();
       }
-      if (e.key === 'ArrowLeft') {
-        anterior()
+      if (e.key === "ArrowLeft") {
+        anterior();
       }
-    })
-  }, [])
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [siguiente, anterior]);
 
   function siguiente() {
     setId((id) => {
-      return (id === max) ? 1 : id + 1
-    })
+      return id === max ? 1 : id + 1;
+    });
   }
 
   function anterior() {
     setId((id) => {
-      return (id === 1) ? max : id - 1
-    })
+      return id === 1 ? max : id - 1;
+    });
   }
 
-  return [personaje, siguiente, anterior]
+  return [personaje, siguiente, anterior];
 }
